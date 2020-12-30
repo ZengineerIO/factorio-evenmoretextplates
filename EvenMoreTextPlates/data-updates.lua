0e05f4e5c2e79d47b8c7a0b515e0f36b0b9d537b
Zengineer <zengineer@endocluster.com>
2020-12-30 09:38:27 +0100

    Version: 1.1.3 Date: 30. 12. 2020   Changes:     - small code cleanup     - finally fixed the "umlaute" in german translation

--- EvenMoreTextPlates/data-updates.lua
+++
@@ -1,6 +0,0 @@
--- update recipes to corrospoding ingredients if they exist
-
---[[ if data.raw.item["textplate-small-creep"] and data.raw.item["biomass"] then
-    data.raw.recipe["textplate-small-creep"].ingredients = {{type = "item", name = "biomass", amount = 1}}
-    data.raw.recipe["textplate-large-creep"].ingredients = {{type = "item", name = "biomass", amount = 4}}
-end ]]
